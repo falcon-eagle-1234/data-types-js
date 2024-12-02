a94ea6b56df2f0442252d2cfad0331bff33996c0
chinbaa <janchivchinbaa@gmail.com>
2024-12-02 16:43:15 +0800

    data types

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("hello world from HTML")
+
+ //data types//
+
+ // 1. string => text type => "text"//
+ // 2. number => number type => 1, 2, 3 //
+ // 3. Booleon => false or true type => false / true //
+ // 4. array
+ // 4. Null or Undifined  => no value//
+
